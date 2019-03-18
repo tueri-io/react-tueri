5ca81ea2ff729183540dae997f9215fd003874c6
Dane Stevens <dane@dsmedia.ca>
2019-03-18 09:29:38 -0400

    change viewport

--- src/Img.js
+++ src/Img.js
@@ -3,79 +3,89 @@ import PropTypes from 'prop-types'
 import { TueriContext } from './Provider'
 import kebabCase from 'lodash.kebabcase'
 
-function Img ({ src, alt, options = {}, format = 'jpg' }) {
+class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' }) {
 
-    const imgRef = React.createRef()
-
-    const [isInViewport, setIsInViewport] = useState(false)
-
-    const [width, setWidth] = useState(0)
-    const [imageLoaded, setImageLoaded] = useState({ thumbnail: false, fullsize: false })
-
-
-    useEffect(() => {
-        setWidth(imgRef.current.clientWidth)
-    })
-
-    // const [currentYOffset, setCurrentYOffset] = useState(0)
-
-    function handleScroll() {
-        
-        // setCurrentYOffset(current)
-
-        // if (!this.inRAF) {
-        //     this.inRAF = true
-        //     window.requestAnimationFrame(() => {
-        //         // this.handleBuffers()
-        //         this.inRAF = false
-        //     })
-        // }
-
-        if (imgRef.current && !imageLoaded.thumbnail) {
-            const windowHeight = window.innerHeight
-            const imageTopPosition = imgRef.current.getBoundingClientRect().top
-            if (windowHeight * 1.5 > imageTopPosition) {
-                setIsInViewport(true)
-            }
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            isInViewport: false,
+            width: 0,
+            height: 0,
+            thumbnailLoaded: false,
+            fullsizeLoaded: false
         }
+
+        this.imgRef = React.createRef()
+
+        this.handleViewport = this.handleViewport.bind(this)
 
     }
 
-    useEffect(() => {        
+    componentDidMount() {
 
-        window.addEventListener('scroll', handleScroll)
+        const width = this.imgRef.current.clientWidth
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
+        this.setState({
+            width
+        })
+        
+        this.handleViewport()
+
+        window.addEventListener('scroll', this.handleViewport)
+
+    }
+
+    // const [currentYOffset, setCurrentYOffset] = useState(0)
+
+    handleViewport() {
+        if (this.imgRef.current && !this.state.thumbnailLoaded) {
+            const windowHeight = window.innerHeight
+            const imageTopPosition = this.imgRef.current.getBoundingClientRect().top
+            if (windowHeight * 1.5 > imageTopPosition) {
+                this.setState({
+                    isInViewport: true
+                })
+            }
+
         }
-    })
+    }
 
-    let queryString = ''
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleViewport)
+    }
 
-    options['scale.width'] = options['scale.width'] || width
+    render() {
 
-    Object.keys(options).map((option, i) => {
-        return queryString +=  `${i < 1 ? '?' : '&'}${option}=${options[option]}`
-    })
+        let queryString = ''
 
-    return(
-        <TueriContext.Consumer>
-            {({ domain, account }) => (
-                <figure className={`tueri__imageContainer ${ imageLoaded.thumbnail || imageLoaded.fullsize ? 'tueri__imageContainer--loaded': '' }`} ref={imgRef}>
-                    {
-                        isInViewport && width > 0 ? (
-                            <React.Fragment>
-                                <img onLoad={ () => { setImageLoaded({ ...imageLoaded, thumbnail: true }) } } className={`tueri__image--thumbnail ${ imageLoaded.thumbnail && !imageLoaded.fullsize ? 'loaded' : ''} ${ imageLoaded.fullsize ? 'fadeOut' : ''}`} src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString.replace(`scale.width=${ width }`, `scale.width=${ Math.round(width * 0.1) }`) }`} alt={ alt } />
-                                <img onLoad={ () => { setImageLoaded({ ...imageLoaded, fullsize: true }) } } className={`tueri__image--fullsize ${ imageLoaded.fullsize ? 'loaded' : ''}`} src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString }`} alt={ alt } />
-                            </React.Fragment>
-                        ) : null
-                    }            
-                </figure>
-            )}
-            
-        </TueriContext.Consumer>
-    )
+        const { src, alt, options = {}, format = 'jpg' } = this.props
+        const { isInViewport, width, thumbnailLoaded, fullsizeLoaded } = this.state
+
+        options['scale.width'] = options['scale.width'] || width
+
+        Object.keys(options).map((option, i) => {
+            return queryString +=  `${i < 1 ? '?' : '&'}${option}=${options[option]}`
+        })
+
+        return(
+            <TueriContext.Consumer>
+                {({ domain, account }) => (
+                    <figure className={`tueri__imageContainer ${ thumbnailLoaded || fullsizeLoaded ? 'tueri__imageContainer--loaded': '' }`} ref={this.imgRef}>
+                        {
+                            isInViewport && width > 0 ? (
+                                <React.Fragment>
+                                    <img onLoad={ () => { this.setState({ thumbnailLoaded: true }) } } className={`tueri__image--thumbnail ${ thumbnailLoaded && !fullsizeLoaded ? 'loaded' : ''} ${ fullsizeLoaded ? 'fadeOut' : ''}`} src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString.replace(`scale.width=${ width }`, `scale.width=${ Math.round(width * 0.1) }`) }`} alt={ alt } />
+                                    <img onLoad={ () => { this.setState({ fullsizeLoaded: true }) } } className={`tueri__image--fullsize ${ fullsizeLoaded ? 'loaded' : ''}`} src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString }`} alt={ alt } />
+                                </React.Fragment>
+                            ) : null
+                        }            
+                    </figure>
+                )}
+                
+            </TueriContext.Consumer>
+        )
+
+    }
 }
 
 Img.propTypes = {
