bad26d12602218fb7910797d82925fa154e15b83
Dane Stevens <dane@dsmedia.ca>
2019-03-19 09:43:49 -0400

    added webp support

--- src/Img.js
+++ src/Img.js
@@ -11,14 +11,14 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
             isInViewport: false,
             width: 0,
             height: 0,
-            thumbnailLoaded: false,
+            lqipLoaded: false,
             fullsizeLoaded: false
         }
 
         this.imgRef = React.createRef()
-        this.handleViewport = this.handleViewport.bind(this)
-
         this.window = typeof window !== 'undefined' && window 
+        this.handleViewport = this.handleViewport.bind(this)       
+        this.isWebpSupported = this.isWebpSupported.bind(this)
 
     }
 
@@ -39,7 +39,7 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
     // const [currentYOffset, setCurrentYOffset] = useState(0)
 
     handleViewport() {
-        if (this.imgRef.current && !this.state.thumbnailLoaded) {
+        if (this.imgRef.current && !this.state.lqipLoaded) {
             const windowHeight = this.window.innerHeight
             const imageTopPosition = this.imgRef.current.getBoundingClientRect().top
             if (windowHeight * 1.5 > imageTopPosition) {
@@ -51,19 +51,35 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
         }
     }
 
+    isWebpSupported() {
+        if (!this.window.createImageBitmap) {
+            return false;
+        }
+        return true;
+    }
+
     componentWillUnmount() {
         this.window.removeEventListener('scroll', this.handleViewport)
     }
 
     render() {
 
-        let queryString = ''
-
+        // Destructure props and state
         const { src, alt, options = {}, format = 'jpg' } = this.props
-        const { isInViewport, width, thumbnailLoaded, fullsizeLoaded } = this.state
+        const { isInViewport, width, fullsizeLoaded } = this.state
 
-        options['scale.width'] = options['scale.width'] || width
+        // Create an empty query string
+        let queryString = ''        
 
+        // If width is specified, otherwise use auto-detected width
+        options['w'] = options['w'] || width
+
+        // If a format has not been specified, detect webp support
+        if (!options['fm'] && this.isWebpSupported) {
+            options['fm'] += 'webp'
+        }
+
+        // Loop through option prop and build queryString
         Object.keys(options).map((option, i) => {
             return queryString +=  `${i < 1 ? '?' : '&'}${option}=${options[option]}`
         })
@@ -73,7 +89,7 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
                 position: 'relative',
                 margin: 0
             },
-            thumbnail: {
+            lqip: {
                 width: '100%',
                 filter: 'blur(5px)',
                 opacity: 1,
@@ -87,16 +103,15 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
             }
         }
 
-        // if (thumbnailLoaded && !fullsizeLoaded) {
-        //     styles.thumbnail.opacity = 1
-        // }
+        // When the fullsize image is loaded, fade out the LQIP
         if (fullsizeLoaded) {
-            styles.thumbnail.opacity = 0
+            styles.lqip.opacity = 0
         }
 
         return(
+            // Return the CDN domain from the TueriProvider
             <TueriContext.Consumer>
-                {({ domain, account }) => (
+                {({ domain }) => (
                     <figure
                         style={ styles.figure }
                         ref={this.imgRef}
@@ -104,17 +119,19 @@ class Img extends React.Component{ // ({ src, alt, options = {}, format = 'jpg' 
                         {
                             isInViewport && width > 0 ? (
                                 <React.Fragment>
+
+                                    {/* Load fullsize image in background */}
                                     <img 
-                                        onLoad={ () => { this.setState({ fullsizeLoaded: true }) } } 
-                                        // className={`tueri__image--fullsize ${ fullsizeLoaded ? 'loaded' : ''}`} 
+                                        onLoad={ () => { this.setState({ fullsizeLoaded: true }) } }
                                         style={ styles.fullsize }
                                         src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString }`}
                                         alt={ alt }
                                     />
+
+                                    {/* Load LQIP in foreground */}
                                     <img 
-                                        onLoad={ () => { this.setState({ thumbnailLoaded: true }) } }
-                                        // className={`tueri__image--thumbnail ${ thumbnailLoaded && !fullsizeLoaded ? 'loaded' : ''} ${ fullsizeLoaded ? 'fadeOut' : ''}`} 
-                                        style={ styles.thumbnail }
+                                        onLoad={ () => { this.setState({ lqipLoaded: true }) } }
+                                        style={ styles.lqip }
                                         src={`${ domain }/${ src }/${ kebabCase(alt) }.${ format }${ queryString.replace(`scale.width=${ width }`, `scale.width=${ Math.round(width * 0.1) }`) }`} 
                                         alt={ alt } 
                                     />
