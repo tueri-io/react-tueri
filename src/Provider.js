9daae81afdf9efad113814f97fd02afc2b9881bb
Dane Stevens <dane@dsmedia.ca>
2019-03-22 06:58:12 -0400

    changed from async/await to promises
    didn't want to rely on babel transform regenerator

--- src/Provider.js
+++ src/Provider.js
@@ -22,8 +22,10 @@ class TueriProvider extends React.Component {
         if (!self.createImageBitmap) return false
 
         const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
-        const blob = await fetch(webpData).then(r => r.blob())
-        return createImageBitmap(blob).then(() => true, () => false)
+        return fetch(webpData)
+        .then(r => r.blob())
+        .then(blob => createImageBitmap(blob).then(() => true, () => false))
+        // return createImageBitmap(blob).then(() => true, () => false)
     }
 
     
