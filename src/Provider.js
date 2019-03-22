72b21b743329c4f53c3d20b7a35d68d7b506db72
Dane Stevens <dane@dsmedia.ca>
2019-03-22 06:41:35 -0400

    rebuilt webp detection to acutally load a webp image

--- src/Provider.js
+++ src/Provider.js
@@ -3,15 +3,53 @@ import PropTypes from 'prop-types'
 
 const TueriContext = React.createContext(true)
 
-function TueriProvider ({ domain = 'https://cdn.tueri.io', accountId, children }) {
+class TueriProvider extends React.Component {
 
-    return(
-        <TueriContext.Provider
-            value={{ domain, accountId }}
-        >
-            { children }
-        </TueriContext.Provider>
-    )
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            supports: {
+                webp: false
+            }
+        }
+
+        this.supportsWebp = this.supportsWebp.bind(this)
+
+    }
+
+    async supportsWebp() {
+        if (!self.createImageBitmap) return false
+
+        const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+        const blob = await fetch(webpData).then(r => r.blob())
+        return createImageBitmap(blob).then(() => true, () => false)
+    }
+
+    
+    componentDidMount() {
+
+        this.supportsWebp()
+        .then((webp) => this.setState({ supports: { ...this.state.supports, webp }, isLoaded: true }))
+
+    }
+
+    render() {
+
+        const { isLoaded, supports } = this.state
+        const { domain = 'https://cdn.tueri.io', accountId, children } = this.props
+
+        if (!isLoaded) return null
+
+        return(
+            <TueriContext.Provider
+                value={{ domain, accountId, supports }}
+            >
+                { children }
+            </TueriContext.Provider>
+        )
+
+    }
 
 }
 
